723ec4bd19c9fdac19939af4ed871e6824b371ee
s-maarouf <maarouf.salahedd@gmail.com>
2024-07-21 16:36:05 +0100

    Created custom setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install zsh -y
+
+./tools/install.sh
--- tools/install.sh
+++ tools/install.sh
@@ -67,7 +67,7 @@ zdot="${ZDOTDIR:-$HOME}"
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-s-maarouf/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
